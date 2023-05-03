ee101dfd6ef1f0371f94070aa24b068b995942eb
dennis githinji <wakahiad@gmail.com>
2023-05-03 14:18:49 +0300

    project setup

---
+++ src/main/java/com/densoft/sdjpaintro/domain/OrderHeader.java
@@ -0,0 +1,45 @@
+package com.densoft.sdjpaintro.domain;
+
+import jakarta.persistence.*;
+@Entity
+public class OrderHeader {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderHeader that = (OrderHeader) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return customer != null ? customer.equals(that.customer) : that.customer == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        return result;
+    }
+}
--- src/main/java/com/densoft/sdjpaintro/repositories/BookNaturalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.densoft.sdjpaintro.repositories;
-
-import com.densoft.sdjpaintro.domain.BookNatural;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookNaturalRepository extends JpaRepository<BookNatural, String> {
-
-}
